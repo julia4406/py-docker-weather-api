356f07a60ffc224390f1345ebd161a3537adee07
Julia Alexeeva <julia4406@gmail.com>
2025-02-18 22:05:59 +0200

    Solution

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.13.2-alpine3.21
+LABEL maintainer="julia4406@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR weather/
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    print("Performing request to Weather API for city Paris...")
+
+    URL = "http://api.weatherapi.com/v1/current.json"
+    key = os.environ["API_KEY"]
+    city = "Paris"
+
+    request_url = f"{URL}?key={key}&q={city}"
+
+    response = requests.get(request_url).json()
+
+    message = (
+        f"{response["location"]["name"]}/{response["location"]["country"]} "
+        f"{response["location"]["localtime"]} Weather: "
+        f"{response["current"]["temp_c"]} Celsius, "
+        f"{response["current"]["condition"]["text"]}"
+    )
+
+    print(message)
 
 
 if __name__ == "__main__":
